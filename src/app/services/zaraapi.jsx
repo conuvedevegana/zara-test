1f08a16d01e30984364f949b9ace5d28af061329
conuvedevegana <cardelsil@gmail.com>
2024-01-29 11:23:23 +0100

    fetch api

---
+++ src/app/services/zaraapi.jsx
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+
+
+export const zaraapi = () => {
+
+
+    const baseUrl = 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json'
+
+    const getAll = async () => {
+        const response = await axios.get(baseUrl);
+        return response;
+    }
+
+    return {
+        getAll
+    }
+}
